ac4e25d30cc746f9b2cccc578f510f82bf152cdf
LucasLeibs <dr.lucasleibs@gmail.com>
2021-02-02 13:32:26 -0800

    user can login and authenticate

--- server/jwtAuth.js
+++ server/jwtAuth.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const pool = require('./db');
 const bcrypt = require('bcrypt');
 const jwtGenerator = require('./utils/jwtGenerate');
-//registering 
-router.post("/register", async (req, res) => {
+const validInfo = require("./middleware/validations")
+
+router.post("/register", validInfo, async (req, res) => {
     try {
         // destructire req body 
         const { name, user_name, email, password} = req.body;
@@ -34,5 +35,32 @@ router.post("/register", async (req, res) => {
         res.status(500).send("Server Error")
     }
 })
+//login 
+router.post("/login", validInfo, async (req, res) => {
+    try {
+        // destructure req body 
+        const {email, password} = req.body;
+      
+        // check if user doesnt exist(if yes throw error)
+        const user = await pool.query("SELECT * FROM users WHERE email = $1",[
+            email
+        ]);
+        if (user.rows.length === 0) {
+            return res.status(401).json("Password or Email is incorrect")
+        }
+        // check credentials
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
+        if (!validPassword) {
+            return res.status(401).json("Password or Email is incorrect")
+        }
+        // give them token
+        const token = jwtGenerator(user.rows[0].user_id);
+        res.json({ token });
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send("Server Error")
+    }
+})
+
 
 module.exports = router;
