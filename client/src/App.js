ae7ec024f7a5bc2f84b5d289d92644f2ab29bb49
LucasLeibs <dr.lucasleibs@gmail.com>
2021-02-11 12:46:57 -0800

    working on dark mode toggle with react switch

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,15 @@ import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
+import {default as Switchy} from "react-switch"
+
 
 function App() {
 const [isAuthenticated, setIsAuthenticated] = useState(false)
+const [checked, setChecked] = useState(false);
+const handleChange = nextChecked => {
+  setChecked(nextChecked);
+};
 
   const setAuth = (boolean) => {
   setIsAuthenticated(boolean)
@@ -16,7 +22,15 @@ const [isAuthenticated, setIsAuthenticated] = useState(false)
   return (
    <Fragment>
      <Router>
-       <div>
+       <div className="static">
+         <div className="absolute top-5 left-5 bg-gray-50">
+         <Switchy
+          onChange={handleChange}
+          checked={checked}
+          className="react-switch"
+          >
+         </Switchy>
+         </div>
        <Switch>
          <Route exact path="/login" render={props => !isAuthenticated ? <Login {...props} setAuth={setAuth} /> : <Redirect to="/home"/>} />
          <Route exact path="/register" render={props => !isAuthenticated ? <Register {...props} setAuth={setAuth} /> : <Redirect to="/login"/>} />
