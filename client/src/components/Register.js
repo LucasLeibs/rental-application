e8f9b0dd7d1ac95a9c9edbacd5e134dc1bb603a9
LucasLeibs <dr.lucasleibs@gmail.com>
2021-02-11 16:06:42 -0800

    user is able to login with the form

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Register() {
-    return (
+export default function Register({setAuth}) {
+const [userInfo, setUserInfo] = useState({ 
+    first_name: "",
+    last_name: "",
+    user_name: "", 
+    email: "",
+    password: ""
+})
+const {first_name, last_name, user_name, email, password} = userInfo;
+
+const onChange = (e) => {
+    setUserInfo({...userInfo, [e.target.name]: e.target.value});
+}
+
+const onSubmitForm = async (e) => {
+    e.preventDefault()
+    try {
+        const body = {first_name, last_name, user_name, email, password }
+        const response = await fetch('http://localhost:5000/authentication/register', {
+            method: 'POST',
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(body)
+        });
+        const parseRes = await response.json()
+        localStorage.setItem("token", parseRes.token)
+        setAuth(true)
+    
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+console.log(userInfo)
+    return   (
        
            <div className="min-h-screen flex items-center justify-center 0bg-gray-5 py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-900">
       <div className="max-w-md w-full space-y-8">
@@ -24,41 +55,63 @@ export default function Register() {
             </a>
           </p>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form onSubmit={onSubmitForm}className="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" value="true"/>
           <div className="rounded-md shadow-sm -space-y-px">
           <div>
-              <label for="name" className="sr-only">
-                Name
+              <label className="sr-only">
+                First Name
               </label>
               <input
-                id="name"
-                name="name"
+              onChange={e => onChange(e)}
+              value={first_name}
+                id="first_name"
+                name="first_name"
                 type="text"
+               
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="First & Last Name"
+                placeholder="First Name"
               />
             </div>
             <div>
-              <label for="email-address" className="sr-only">
-                Email address
+              <label className="sr-only">
+                Last Name
               </label>
               <input
-                id="email-address"
-                name="email"
-                type="email"
-                autocomplete="email"
+                 onChange={e => onChange(e)}
+                 value={last_name}
+                id="last_name"
+                name="last_name"
+                type="text"
+               
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
+                placeholder="Last Name"
               />
             </div>
             <div>
-              <label for="email-address" className="sr-only">
-                Email address
+              <label  className="sr-only">
+                User Name
               </label>
               <input
+                id="user-name"
+                name="user_name"
+                type="text"
+                onChange={e => onChange(e)}
+                value={user_name}
+                required
+                className="mt-3 mb-3 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Username"
+              />
+            </div>
+            <div>
+              <label className="sr-only">
+                
+              </label>
+              <input
+                 onChange={e => onChange(e)}
+                 value={email}
                 id="email-address"
                 name="email"
                 type="email"
@@ -69,10 +122,12 @@ export default function Register() {
               />
             </div>
             <div>
-              <label for="password" className="sr-only">
+              <label className="sr-only">
                 Password
               </label>
               <input
+                 onChange={e => onChange(e)}
+                 value={password}
                 id="password"
                 name="password"
                 type="password"
