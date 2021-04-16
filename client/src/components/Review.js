c7cfb770ab19ce7e881a2192f0d7e02edb85cc74
LucasLeibs <dr.lucasleibs@gmail.com>
2021-04-16 13:38:53 -0700

    added slideshow into review

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import Slideshow from './Slideshow'
 export default function Review() {
     return (
         <div className="static min-h-screen p-5 flex items-start flex-col justify-start bg-none 
         px-4 sm:px-6 lg:px-8 dark:bg-gray-900 dark:text-white">
             <div className="w-12/12 mt-20 float-left">
-                <h1>Address</h1>
-                <p>City, State</p>
+                <h1 className="text-indigo-600 font-extrabold text-5xl">5633 Melendy Dr</h1>
+                <p className="mt-2 text-indigo-600 font-extrabold text-1xl">Langley, Washington</p>
+            </div>
+            <div>
+            <Slideshow/>
             </div>
         
         </div>
--- client/src/components/Slideshow.js
+++ client/src/components/Slideshow.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import homeCheck from "../images/house-check.png";
 import createReview from "../images/holding-the-phone.png";
 import community from "../images/community.png";
-const images = [homeCheck, createReview, community];
+const images = ['https://images.adsttc.com/media/images/5014/01bd/28ba/0d3b/4500/0b8c/large_jpg/stringio.jpg?1414576505', 'https://s3-prod.chicagobusiness.com/%231_57.jpg', 'https://miro.medium.com/max/619/0*rB62Gty4XcUO1DUt.jpg'];
 export default function Slideshow() {
   const [currentImage, changeImage] = useState(0);
 
@@ -21,10 +21,12 @@ export default function Slideshow() {
     }
   };
   return (
-    <div className="mt-20 relative w-100 flex items-center flex-col justify-center">
+    <div className="mt-20 relative w-6/12 flex items-center flex-col justify-center">
       <div className="w-100 flex items-center flex-col justify-center">
-        <img className="w-3/12" src={images[currentImage]}></img>
-        <button className="bg-indigo-600 p-2 m-1 rounded-md text-white" onClick={() => next()}>Next</button>
+        <img className="w-100" src={images[currentImage]}></img>
+      </div>
+      <div>
+      <button className="bg-indigo-600 p-2 m-1 rounded-md text-white" onClick={() => next()}>Next</button>
         <button className="bg-indigo-600 p-2 m-1 rounded-md text-white" onClick={() => back()}>Back</button>
         <p>{currentImage + 1} / {images.length}</p>
       </div>
